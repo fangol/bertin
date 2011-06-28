b4e9da770bfc0abcfdc9f59cd34ef96f4265a11a
mathieu <mathieu@ASSISI.(none)>
2011-06-28 15:27:53 -0400

    Platform specific SWT dependency.

--- project.clj
+++ project.clj
@@ -1,7 +1,13 @@
-(defproject bertin "1.0.0-SNAPSHOT"
-  :description "Semiology of Graphics"
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-                 [org.eclipse/swt-cocoa-macosx-x86_64 "3.5.2"]]
-  :jvm-opts ["-Xmx1g" "-XstartOnFirstThread"]
-  :main bertin.core)
+(let [swt (symbol (str
+           "org.eclipse/swt"
+           "-" (let [name (System/getProperty "os.name")]
+                 (cond
+                  (.startsWith name "Windows") "win32-win32"
+                  (= name "Mac OS X") "cocoa-macosx"))
+           "-" (System/getProperty "os.arch")))]
+  (defproject bertin "1.0.0-SNAPSHOT"
+    :description "Semiology of Graphics"
+    :dependencies [[org.clojure/clojure "1.2.1"]
+                   [org.clojure/clojure-contrib "1.2.0"]
+                   [~swt "3.5.2"]]
+    :main bertin.core))
