1703abf9b9acafa941bd0be879d1bffe2008cf9f
mathieu <mathieu@ASSISI.(none)>
2011-07-04 15:33:42 -0400

    Leiningen project target 'applet'.

--- project.clj
+++ project.clj
@@ -4,10 +4,14 @@
                  (cond
                   (.startsWith name "Windows") "win32-win32"
                   (= name "Mac OS X") "cocoa-macosx"))
-           "-" (System/getProperty "os.arch")))]
+           "-" (System/getProperty "os.arch")))
+      target (if
+               (= *command-line-args* '("uberjar" "applet"))
+               (symbol "bertin.applet")
+               (symbol "bertin.core"))]
   (defproject bertin "1.0.0-SNAPSHOT"
     :description "Semiology of Graphics"
     :dependencies [[org.clojure/clojure "1.2.1"]
                    [org.clojure/clojure-contrib "1.2.0"]
                    [~swt "3.5.2"]]
-    :main bertin.core))
+    :main ~target))
